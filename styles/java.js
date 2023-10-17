3f5a46e3b42375e8ab1ee7a8f252d9c9fff00fb6
pletschthai <146131783+pletschthai@users.noreply.github.com>
2023-10-17 15:28:59 -0600

    Trying to make my forecast section work

--- styles/java.js
+++ styles/java.js
@@ -51,6 +51,8 @@ function displayResults(response) {
   );
   const formattedTimezone = getFormattedTime(response.data.timezone);
   getForecast(response.data.coord);
+
+  displayForecast;
 }
 
 function getFormattedTime(timezoneOffset) {
@@ -108,42 +110,46 @@ function formatDay(timestamp) {
 }
 function displayForecast(response) {
   let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = `<div class="row mt-3">`;
-  forecast.forEach(function (forecastDate, index) {
-    if (index > 0 && index < 7) {
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
       forecastHTML =
         forecastHTML +
         `
       <div class="col-2">
-      <div class= dayForecast>
-        <div class="weather-forecast-date">${formatDay(forecastDate.dt)}</div>
-        <img src="${getIconSrc(
-          forecastDate.weather[0].icon
-        )}" alt="Clear" id="icon" class="float-left" />
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
         <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature-max"> ${Math.round(
-            forecastDate.temp.max
+            forecastDay.temp.max
           )}° </span>
           <span class="weather-forecast-temperature-min"> ${Math.round(
-            forecastDate.temp.min
+            forecastDay.temp.min
           )}° </span>
-        </div>
         </div>
       </div>
   `;
     }
   });
+
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
 }
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "7059cb165caa3316bff682d263a01b1e";
+  let apiKey = "c76db1bd2c2a808bab15d20555e59a59";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
-search("New York");
+
+search("San Francisco");
